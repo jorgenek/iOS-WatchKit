25e6ee448e81c535f36ec30b7c12a60869aec129
J√∏rgen Ekeland <jorgen.ekeland@itera.no>
2015-10-05 17:21:37 +0200

    Initial Commit

---
+++ FirstWatchKitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstWatchKitApp
+//
+//  Created by Jørgen Ekeland on 05/10/15.
+//  Copyright © 2015 Jørgen Ekeland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
